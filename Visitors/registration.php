56394e222fed863a625b414e349c24670dbf30a4
mr_bushenyula <bushenyulatheophile@gmail.com>
2023-07-05 06:45:40 +0100

    modified backend all pages

--- Visitors/registration.php
+++ Visitors/registration.php
@@ -4,20 +4,24 @@ $first_Name= $_POST['fname'];
 $last_name=$_POST['lname'];
 $contact=$_POST['Contact'];
 $reg_number= $_POST['reGnumber'];
-
+$code=$_POST['code'];
+echo $codes;
 include 'connection.php';
-
-
-$sql= "INSERT INTO students_data (fName,sName,phoneContact,registration_number)  VALUES ('$first_Name','$last_name',$contact,'$reg_number')";
+if(isset($_POST['generate'])){
+   
+}
+if($code=$codes){
+$sql= "INSERT INTO students_data (fName,sName,phoneContact,registration_number,code	) 
+ VALUES ('$first_Name','$last_name',$contact,'$reg_number','$code')";
 $query= mysqli_query ($connect,$sql);
 if($query){
     echo 'inserted seccesfuly';
-    // header('location:display.php');
+    header('location:dashboard.php');
 }else{
     // echo 'eror while insertig try again';  
       echo 'faild to insert';             
 }
-
+}
 
 
 ?>
